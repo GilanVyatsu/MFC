fb28d7affbd0741986a2bc62d651958cb66edc9b
Dmitriy <Dmitriy@SCORPIO-DM>
2020-06-22 23:06:20 +0300

    MFC

--- MFC/FormMain.Designer.cs
+++ MFC/FormMain.Designer.cs
@@ -33,10 +33,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.buttonContacts = new System.Windows.Forms.Button();
+            this.buttonJobs = new System.Windows.Forms.Button();
+            this.buttonServices = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -81,52 +80,42 @@
             this.button1.Text = "О МФЦ";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // buttonContacts
             // 
-            this.button2.Location = new System.Drawing.Point(513, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonContacts.Location = new System.Drawing.Point(321, 165);
+            this.buttonContacts.Name = "buttonContacts";
+            this.buttonContacts.Size = new System.Drawing.Size(142, 52);
+            this.buttonContacts.TabIndex = 4;
+            this.buttonContacts.Text = "Контакты";
+            this.buttonContacts.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // buttonJobs
             // 
-            this.button3.Location = new System.Drawing.Point(445, 194);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonJobs.Location = new System.Drawing.Point(321, 249);
+            this.buttonJobs.Name = "buttonJobs";
+            this.buttonJobs.Size = new System.Drawing.Size(142, 55);
+            this.buttonJobs.TabIndex = 5;
+            this.buttonJobs.Text = "Вакансии";
+            this.buttonJobs.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // buttonServices
             // 
-            this.button4.Location = new System.Drawing.Point(594, 121);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(594, 223);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.buttonServices.Location = new System.Drawing.Point(321, 336);
+            this.buttonServices.Name = "buttonServices";
+            this.buttonServices.Size = new System.Drawing.Size(142, 54);
+            this.buttonServices.TabIndex = 6;
+            this.buttonServices.Text = "Услуги";
+            this.buttonServices.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(921, 570);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(516, 445);
+            this.Controls.Add(this.buttonServices);
+            this.Controls.Add(this.buttonJobs);
+            this.Controls.Add(this.buttonContacts);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -145,10 +134,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button buttonContacts;
+        private System.Windows.Forms.Button buttonJobs;
+        private System.Windows.Forms.Button buttonServices;
     }
 }
 
